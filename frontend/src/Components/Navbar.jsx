3dd7c6ff4d8c86ed87ef94275c4ff1dd3f3e669a
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-05-03 01:20:09 +0530

    Navbar updated furthermore

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import logo1 from '../../public/logo2.png';
 import { IoLanguage } from "react-icons/io5";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 
 
 const Navbar = () => {
+
+    const [isMenuOpen, setIsMenuOpen] =  useState(false);
+
+    const toggleMenu = ()=>{
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     const navItems = [
         {link:"Overview", path:"home"},
@@ -13,21 +22,21 @@ const Navbar = () => {
     ]
 
   return (
-    <nav>
+    <nav className="bg-white md:px-14 p-4 max-w-screen-2xl mx-auto text-primary">
         <div className="text-lg container mx-auto flex justify-between items-center font-medium">
             <div className="flex items-center space-x-14 pt-3">
                 <a href="/" className="text-2xl font-semibold flex items-center space-x-3 pl-4  text-primary"><img src={logo1} alt="logo1" className="w-10 inline-block items-center"/><span>NexPlan</span></a>
 
                 {/* showing nav Items */}
 
-                <ul className="md:flex space-x-14">
+                <ul className="md:flex space-x-14 hidden">
                 
                     {
                         navItems.map(({link,path})=> 
                         <a key={link} href={path} className="block hover:text-gray-300">{link}</a>
                         )
                     }
-                    
+
                 </ul>
             </div>
 
@@ -37,9 +46,33 @@ const Navbar = () => {
                 <a href="/" className="hidden lg:flex items-center hover:text-secondary"><IoLanguage className="mr-2"/><span>Languages</span></a>
                 <button className="bg-secondary py-2 px-4 transition-all duration-300 rounded hover:text-white hover:bg-indigo-600">Sign Up</button>
             </div>
+
+            {/* menu btn only display to mobile divices */}
+
+            <div className="md:hidden">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none focus:text-gray-300">
+                        {
+                            isMenuOpen ? (<IoCloseCircleOutline className="w-6 h-6 text-primary"/>) : (<IoMenu className="w-6 h-6 text-primary"/>)
+                        } 
+                    </button>
+
+            </div>
+
+
         </div>
       
     </nav>
+
+    // navitem display for mobile divices
+
+    <div className= {`space-y-4 px-4 pt-5 pb-5 bg-secondary ${isMenuOpen ? "block fixed top-0 right-0 left-0":"hidden" } `}>
+    {
+        navItems.map(({link, path})=>
+        <a key={link} href={path} className="block hover:text-gray-300">{link}</a>
+        )
+    }
+
+    </div>
   )
 }
 
